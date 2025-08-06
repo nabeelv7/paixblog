c839d0569f58c5dda34aa0837cbdd7f57ec4ac08
nabeelv7 <nabeelsadiq.dev@gmail.com>
2025-08-06 18:55:30 +0500

    added no blogs found

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,11 +10,13 @@
     let blogPreviews = $state([]);
 
     onMount(() => {
-        blogPreviews = data.blogs.map((blog) => {
-            const raw = marked.parse(blog.bodyPreview || "");
-            const safe = DOMPurify.sanitize(`<div>${raw}</div>`);
-            return { ...blog, previewHtml: safe };
-        });
+        if (data.blog.length > 0) {
+            blogPreviews = data.blogs.map((blog) => {
+                const raw = marked.parse(blog.bodyPreview || "");
+                const safe = DOMPurify.sanitize(`<div>${raw}</div>`);
+                return { ...blog, previewHtml: safe };
+            });
+        }
     });
 </script>
 
@@ -27,26 +29,30 @@
     </h1>
 
     <!-- Blog List (client-only after onMount) -->
-    {#if blogPreviews.length > 0}
-        <section class="flex flex-col w-full gap-2 mt-10">
-            {#each blogPreviews as blog}
-                <a href={`/blog/${blog.id}`}>
-                    <div
-                        class="w-full space-y-2 text-left py-5 rounded-xs hover:bg-zinc-800 hover:pl-10 transition-all duration-250 ease-out text-wrap"
-                    >
-                        <h1
-                            class="text-xl font-medium text-shadow-xs text-shadow-gray-400"
+    {#if data?.blogs.length > 0}
+        {#if blogPreviews.length > 0}
+            <section class="flex flex-col w-full gap-2 mt-10">
+                {#each blogPreviews as blog}
+                    <a href={`/blog/${blog.id}`}>
+                        <div
+                            class="w-full space-y-2 text-left py-5 rounded-xs hover:bg-zinc-800 hover:pl-10 transition-all duration-250 ease-out text-wrap"
                         >
-                            {blog.title}
-                        </h1>
-                        <div class="opacity-80 text-balance break-words">
-                            {@html blog.previewHtml}
+                            <h1
+                                class="text-xl font-medium text-shadow-xs text-shadow-gray-400"
+                            >
+                                {blog.title}
+                            </h1>
+                            <div class="opacity-80 text-balance break-words">
+                                {@html blog.previewHtml}
+                            </div>
                         </div>
-                    </div>
-                </a>
-            {/each}
-        </section>
+                    </a>
+                {/each}
+            </section>
+        {:else}
+            <p class="opacity-60 italic mt-10">Loading blogs..</p>
+        {/if}
     {:else}
-        <p class="opacity-60 italic mt-10">Loading blogs...</p>
+        <p class="opacity-60 italic mt-10">No blogs found.
     {/if}
 </main>
